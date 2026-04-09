5ba38ce78ccbab9da1246d0b29457b3eb61ca026
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-04-09 21:34:02 +0100

    removed some non critical errors

--- Banner/scripts/dynamicAdding2.js
+++ Banner/scripts/dynamicAdding2.js
@@ -1,11 +1,11 @@
-box = document.getElementsByClassName("box")
+let box = document.getElementsByClassName("box")
 
 function DynamicPopulating(amount,content,clear){
     if(clear){
         box[0].innerHTML = "";
     }
     for (let i = 0; i < amount; i++) {
-        number = document.createElement("span")
+        let number = document.createElement("span")
         number.textContent = content
         if(number.textContent=="."){
             number.style.fontSize = "40px"
@@ -13,23 +13,23 @@ function DynamicPopulating(amount,content,clear){
             number.style.fontSize = "20px"
         }
         number.style.color = "black"
-        const computedStyles = window.getComputedStyle(box[0]);
-        maxDisplacementHorizontal = parseFloat(computedStyles.width)-60
-        randomisedplacement = Math.floor((Math.random()*maxDisplacementHorizontal)+1)
-        makeIntoString = randomisedplacement+"px"
+        const computedStyles = globalThis.getComputedStyle(box[0]);
+        let maxDisplacementHorizontal = Number.parseFloat(computedStyles.width)-60
+        let randomisedplacement = Math.floor((Math.random()*maxDisplacementHorizontal)+1)
+        let makeIntoString = randomisedplacement+"px"
 
-        maxDisplacementVertical = parseFloat(computedStyles.height) - 60
-        randomisedplacement2 = Math.floor((Math.random()*maxDisplacementVertical)+1)
-        makeIntoString2 = randomisedplacement2+"px"
+        let maxDisplacementVertical = Number.parseFloat(computedStyles.height) - 60
+        let randomisedplacement2 = Math.floor((Math.random()*maxDisplacementVertical)+1)
+        let makeIntoString2 = randomisedplacement2+"px"
 
 
         number.style.position = "absolute"
         number.style.top = makeIntoString2
         number.style.left = makeIntoString
-        Name = "spin"
-        Duration = 3+"s"
-        Iterations = "infinite"
-        TimingFunction = "linear"
+        let Name = "spin"
+        let Duration = 3+"s"
+        let Iterations = "infinite"
+        let TimingFunction = "linear"
         number.style.animationName = Name
         number.style.animationDuration = Duration
         number.style.animationIterationCount = Iterations
@@ -72,13 +72,13 @@ button.addEventListener("click", () => {
     runCommand()
 })
 
-picker = document.getElementById("colourPicker")
+let picker = document.getElementById("colourPicker")
 picker.addEventListener("input", () => {
     box[0].style.backgroundColor = picker.value
     box[0].style.borderColor = picker.value
 })
 
-Resetbackground = document.getElementById("reset")
+let Resetbackground = document.getElementById("reset")
 Resetbackground.addEventListener("click", () => {
     box[0].style.backgroundColor = "#bed4df"
     box[0].style.borderColor = "#bed4df"
